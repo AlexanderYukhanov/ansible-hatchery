cb52b143ad8bb170fa89262efae9bbe73755853c
zikalino <zikalino@microsoft.com>
2017-12-05 20:13:18 +0800

    script to create pr

---
+++ pr.sh
@@ -0,0 +1,14 @@
+if [ ! -d "~/$1" ]; then
+    echo "CLONING ansible repo"
+    git clone --recursive https://github.com/VSChina/ansible.git ~/$1
+    cd ~/$1
+    git checkout $1 2>/dev/null || git checkout -b $1;
+fi
+
+cd ~/$1
+git pull
+cp ~/ansible-hatchery/python/lib/ansible/modules/cloud/azure/$1.py ~/$1/lib/ansible/modules/cloud/azure/$1.py
+cp -v -r ~/ansible-hatchery/python/test/integration/targets/$1 ~/$1/test/integration/targets
+git add -A
+git commit -m "[new module] Adding $1 /cloud/azure/$1"
+git push --set-upstream origin $1
