d42f460b9e6ad47b3320bcf71e1523a10b4ce7f0
zikalino <zikalino@microsoft.com>
2017-12-06 10:45:15 +0800

    added init & role prs

---
+++ role-pr.sh
@@ -0,0 +1,10 @@
+
+cd ~/preview-modules
+git checkout hatchery-pr-branch 2>/dev/null || git checkout -b hatchery-pr-branch;
+
+cp -rfv ~/ansible-hatchery/role/modules/* ~/preview-modules/library
+cp -rfv ~/ansible-hatchery/role/tests/* ~/preview-modules/tests/integration/targets
+
+git add -A
+git commit -m "updated modules"
+git push --set-upstream origin hatchery-pr-branch
