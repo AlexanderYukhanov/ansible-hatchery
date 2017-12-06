7d519256257d6cd10d831d6e8dff4a7d11e1f39c
zikalino <zikalino@microsoft.com>
2017-12-06 10:16:10 +0800

    added generate cmd

---
+++ generate.cmd
@@ -0,0 +1,25 @@
+
+rmdir /S /Q c:\dev\ansible-hatchery\prs
+rmdir /S /Q c:\dev\ansible-hatchery\role
+rmdir /S /Q c:\dev\ansible-hatchery\all
+
+
+cd c:\dev\azure-rest-api-specs\specification\sql\resource-manager
+call autorest --output-folder=c:\dev\ansible-hatchery --use=c:\dev\autorest.ansible\ --python --tag=package-2017-03-preview
+
+xcopy /S c:\dev\ansible-hatchery\python\prs\* c:\dev\ansible-hatchery\prs\
+xcopy /S c:\dev\ansible-hatchery\python\role\* c:\dev\ansible-hatchery\role\
+xcopy /S c:\dev\ansible-hatchery\python\all\* c:\dev\ansible-hatchery\all\
+xcopy /S c:\dev\ansible-hatchery\python\template\* c:\dev\ansible-hatchery\template\
+rmdir /S /Q c:\dev\ansible-hatchery\python
+
+cd c:\dev\azure-rest-api-specs\specification\mysql\resource-manager
+call autorest --output-folder=c:\dev\ansible-hatchery\ --use=c:\dev\autorest.ansible\ --python --tag=package-2017-04-preview
+
+cd c:\dev\azure-rest-api-specs\specification\postgresql\resource-manager
+call autorest --output-folder=c:\dev\ansible-hatchery\ --use=c:\dev\autorest.ansible\ --python --tag=package-2017-04-preview
+
+cd c:\dev\azure-rest-api-specs\specification\authorization\resource-manager
+call autorest --output-folder=c:\dev\ansible-hatchery\ --use=c:\dev\autorest.ansible\ --python --tag=package-2015-07
+
+
