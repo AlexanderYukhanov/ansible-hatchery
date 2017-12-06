9e78b37705d79f7abcf404d36637c13cab2cd7d8
zikalino <zikalino@microsoft.com>
2017-12-07 06:55:01 +0800

    updated test scripts

--- sanity.sh
+++ sanity.sh
@@ -5,8 +5,8 @@ for f in $1*
 do
   echo "Processing $f"
   name=$(echo $f | cut -f 1 -d '.')
-  cp -v ~/ansible-hatchery/python/lib/ansible/modules/cloud/azure/$name.py ~/ansible/lib/ansible/modules/cloud/azure/$name.py
-  cp -v -r ~/ansible-hatchery/python/test/integration/targets/$name ~/ansible/test/integration/targets 
+  cp -v ~/ansible-hatchery/all/modules/$name.py ~/ansible/lib/ansible/modules/cloud/azure/$name.py
+  cp -v -r ~/ansible-hatchery/all/tests/$name ~/ansible/test/integration/targets 
 
   ansible-test sanity -v --local --python 2.7 $name
 
