1fd35ee0ea86d1f0a9a2b13f8d9cb40c60d6d04d
zikalino <zikalino@microsoft.com>
2017-12-07 14:36:32 +0800

    another try

--- all/modules/azure_rm_authorizationroleassignment.py
+++ all/modules/azure_rm_authorizationroleassignment.py
@@ -141,9 +141,11 @@ class AzureRMRoleAssignments(AzureRMModuleBase):
             if hasattr(self, key):
                 setattr(self, key, kwargs[key])
             elif key == "properties_role_definition_id":
-                self.parameters['properties']["roleDefinitionId"] = kwargs[key]
+                self.parameters['properties']["role_definition_id"] = kwargs[key]
             elif key == "properties_principal_id":
                 self.parameters['properties']["principal_id"] = kwargs[key]
+
+            self.parameters['properties']["role_definition_id"] = "XYZ"
 
         old_response = None
         results = dict()
