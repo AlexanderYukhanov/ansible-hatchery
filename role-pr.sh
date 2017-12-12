a4e6bde49c9b5838a0a16c7bc818cb0bf1b0b16b
zikalino <zikalino@microsoft.com>
2017-12-12 09:43:10 +0800

    updated script for preview modules

--- role-pr.sh
+++ role-pr.sh
@@ -1,11 +1,11 @@
 cd ~/ansible-hatchery
 git pull
 
-cd ~/preview-modules
+cd ~/azure_preview_modules
 git checkout hatchery-pr-branch 2>/dev/null || git checkout -b hatchery-pr-branch;
 
-cp -rfv ~/ansible-hatchery/role/modules/* ~/preview-modules/library
-cp -rfv ~/ansible-hatchery/role/tests/* ~/preview-modules/tests/integration/targets
+cp -rfv ~/ansible-hatchery/role/modules/* ~/azure_preview_modules/library
+cp -rfv ~/ansible-hatchery/role/tests/* ~/azure_preview_modules/tests/integration/targets
 
 git add -A
 git commit -m "updated modules"
