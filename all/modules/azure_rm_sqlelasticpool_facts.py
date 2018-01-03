6cccda9a72ed763cb74246a73503dc374e6476c6
zikalino <zikalino@microsoft.com>
2018-01-03 13:43:02 +0800

    fix elastic pool thingy

--- all/modules/azure_rm_sqlelasticpool_facts.py
+++ all/modules/azure_rm_sqlelasticpool_facts.py
@@ -126,10 +126,10 @@ class AzureRMElasticPoolsFacts(AzureRMModuleBase):
                 self.elastic_pool_name is not None and
                 self.filter is not None):
             self.results['ansible_facts']['list_metrics'] = self.list_metrics()
-        elif (self.resource_group is not None and
-              self.server_name is not None and
-              self.elastic_pool_name is not None):
-            self.results['ansible_facts']['list_metric_definitions'] = self.list_metric_definitions()
+        #elif (self.resource_group is not None and
+        #      self.server_name is not None and
+        #      self.elastic_pool_name is not None):
+        #    self.results['ansible_facts']['list_metric_definitions'] = self.list_metric_definitions()
         elif (self.resource_group is not None and
               self.server_name is not None and
               self.elastic_pool_name is not None):
