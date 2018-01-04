fe45b16028b7020380a9962f852706b79030d74f
zikalino <zikalino@microsoft.com>
2018-01-04 13:14:04 +0800

    final fix?

--- all/modules/azure_rm_containerregistryreplication.py
+++ all/modules/azure_rm_containerregistryreplication.py
@@ -226,7 +226,7 @@ class AzureRMReplications(AzureRMModuleBase):
                 response = self.mgmt_client.replications.update(resource_group_name=self.resource_group,
                                                                 registry_name=self.registry_name,
                                                                 replication_name=self.replication_name,
-                                                                replication_update_parameters=self.replication_update_parameters)
+                                                                location=self.location)
             if isinstance(response, AzureOperationPoller):
                 response = self.get_poller_result(response)
 
