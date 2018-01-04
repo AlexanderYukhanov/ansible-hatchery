3678b888325b2625aa0dc251a16efb67922b7133
zikalino <zikalino@microsoft.com>
2018-01-04 13:50:15 +0800

    fixing webhook

--- all/modules/azure_rm_containerregistrywebhook.py
+++ all/modules/azure_rm_containerregistrywebhook.py
@@ -194,8 +194,8 @@ class AzureRMWebhooks(AzureRMModuleBase):
 
         resource_group = self.get_resource_group(self.resource_group)
 
-        if "location" not in self.parameters:
-            self.parameters["location"] = resource_group.location
+        #if "location" not in self.parameters:
+        #    self.parameters["location"] = resource_group.location
 
         old_response = self.get_webhooks()
 
