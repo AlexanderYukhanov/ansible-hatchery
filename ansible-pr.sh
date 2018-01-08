cb30ea9b9715f65e61b3585fa11b0b9b982a8585
Ubuntu <zim@zims-ubuntu-3.hzdriptz0rguzhq25yp3koz2nd.hx.internal.cloudapp.net>
2018-01-08 04:54:53 +0000

    test

--- ansible-pr.sh
+++ ansible-pr.sh
@@ -7,8 +7,8 @@ fi
 
 cd ~/$1
 git pull
-cp ~/ansible-hatchery/prs/modules/$1.py ~/$1/lib/ansible/modules/cloud/azure/$1.py
-cp -v -r ~/ansible-hatchery/prs/tests/$1 ~/$1/test/integration/targets
+cp ~/ansible-hatchery/role/modules/$1.py ~/$1/lib/ansible/modules/cloud/azure/$1.py
+cp -v -r ~/ansible-hatchery/role/tests/$1 ~/$1/test/integration/targets
 git add -A
 git commit -m "updates to $1"
 git push --set-upstream origin $1
