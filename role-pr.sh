7b962e2c8f15f47cfada1f6ce0256fb568d649c4
zikalino <zikalino@microsoft.com>
2018-01-08 13:13:24 +0800

    one fix

--- role-pr.sh
+++ role-pr.sh
@@ -9,4 +9,4 @@ cp -rfv ~/ansible-hatchery/role/tests/* ~/azure_preview_modules/tests/integratio
 
 git add -A
 git commit -m "updated modules"
-git push https://$GIT_USER:$GIT_PASSWORD@github.com/zikalino/ansible-hatchery.git --set-upstream origin hatchery-pr-branch
+git push --repo=https://$GIT_USER:$GIT_PASSWORD@github.com/zikalino/ansible-hatchery.git --set-upstream origin hatchery-pr-branch
