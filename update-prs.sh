ea5d574823f10298932c6397b8e55ff5a44aec11
zikalino <zikalino@microsoft.com>
2018-01-09 13:11:09 +0800

    add more prs

--- update-prs.sh
+++ update-prs.sh
@@ -1,4 +1,15 @@
-
+./ansible-pr.sh azure_rm_sqlserver_facts
+./ansible-pr.sh azure_rm_mysqlserver_facts
+./ansible-pr.sh azure_rm_postgresqlserver_facts
 ./ansible-pr.sh azure_rm_sqldatabase
 ./ansible-pr.sh azure_rm_mysqldatabase
 ./ansible-pr.sh azure_rm_postgresqldatabase
+./ansible-pr.sh azure_rm_sqldatabase_facts
+./ansible-pr.sh azure_rm_mysqldatabase_facts
+./ansible-pr.sh azure_rm_postgresqldatabase_facts
+./ansible-pr.sh azure_rm_sqlfirewallrule
+./ansible-pr.sh azure_rm_mysqlfirewallrule
+./ansible-pr.sh azure_rm_postgresqlfirewallrule
+./ansible-pr.sh azure_rm_sqlfirewallrule_facts
+./ansible-pr.sh azure_rm_mysqlfirewallrule_facts
+./ansible-pr.sh azure_rm_postgresqlfirewallrule_facts
