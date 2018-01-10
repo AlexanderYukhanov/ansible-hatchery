8f462183ae39a15721ceb87d86df6a0f2b2dd824
zikalino <zikalino@microsoft.com>
2018-01-10 10:48:18 +0800

    removed expand

--- all/modules/azure_rm_sqldatabase_facts.py
+++ all/modules/azure_rm_sqldatabase_facts.py
@@ -32,9 +32,6 @@ options:
     filter:
         description:
             - An OData filter expression that describes a subset of metrics to return.
-    expand:
-        description:
-            - A comma separated list of child objects to expand in the response. Possible properties: serviceTierAdvisors, transparentDataEncryption.
     elastic_pool_name:
         description:
             - The name of the elastic pool to be retrieved.
