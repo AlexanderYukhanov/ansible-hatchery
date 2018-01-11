13935191a911b9dfd4affa05c4c7dcbfa79dc4a0
zikalino <zikalino@microsoft.com>
2018-01-11 12:36:58 +0800

    fixed paths

--- role-sanity.sh
+++ role-sanity.sh
@@ -7,8 +7,8 @@ for f in $1*
 do
   echo "Processing $f"
   name=$(echo $f | cut -f 1 -d '.')
-  cp -v ~/ansible-hatchery/role/modules/$name.py ~/ansible/lib/ansible/modules/cloud/azure/$name.py
-  cp -v -r ~/ansible-hatchery/role/tests/$name ~/ansible/test/integration/targets 
+  cp -v ~/ansible-hatchery/azure_preview_modules/library/$name.py ~/ansible/lib/ansible/modules/cloud/azure/$name.py
+  cp -v -r ~/ansible-hatchery/azure_preview_modules/tests/integration/targets/$name ~/ansible/test/integration/targets 
 
   ansible-test sanity -v --local --python 2.7 $name
 
