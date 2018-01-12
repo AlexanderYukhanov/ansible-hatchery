133eed12d6d587c63aa108a224567032b87a007a
zikalino <zikalino@microsoft.com>
2018-01-12 12:36:43 +0800

    fixed databases

--- role/modules/azure_rm_postgresqldatabase.py
+++ role/modules/azure_rm_postgresqldatabase.py
@@ -171,7 +171,7 @@ class AzureRMDatabases(AzureRMModuleBase):
                     self.to_do = Actions.Update
                 if ('charset' in self.parameters) and (self.parameters['charset'] != old_response['charset']):
                     self.to_do = Actions.Update
-                if self.to_do == Actions.Update
+                if self.to_do == Actions.Update:
                     self.delete_postgresqldatabase()
 
         if (self.to_do == Actions.Create) or (self.to_do == Actions.Update):
