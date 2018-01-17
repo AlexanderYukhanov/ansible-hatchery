c1f06035d45bc497b19ec2654ce18236fdd9c108
zikalino <zikalino@microsoft.com>
2018-01-17 13:28:55 +0800

    changed pr update script

--- update-prs.sh
+++ update-prs.sh
@@ -1,20 +1,4 @@
-./ansible-pr.sh azure_rm_mysqlserver_facts
-./ansible-pr.sh azure_rm_postgresqlserver_facts
-./ansible-pr.sh azure_rm_sqldatabase
-./ansible-pr.sh azure_rm_sqldatabase_facts
-./ansible-pr.sh azure_rm_mysqldatabase_facts
-./ansible-pr.sh azure_rm_postgresqldatabase_facts
-./ansible-pr.sh azure_rm_sqlfirewallrule
-./ansible-pr.sh azure_rm_mysqlfirewallrule
-./ansible-pr.sh azure_rm_postgresqlfirewallrule
-./ansible-pr.sh azure_rm_sqlfirewallrule_facts
-./ansible-pr.sh azure_rm_mysqlfirewallrule_facts
-./ansible-pr.sh azure_rm_postgresqlfirewallrule_facts
-
-./ansible-pr.sh azure_rm_containerregistry_facts
-./ansible-pr.sh azure_rm_containerinstance_facts
-
-./ansible-pr.sh azure_rm_containerregistryreplication
-./ansible-pr.sh azure_rm_containerregistryreplication_facts
-./ansible-pr.sh azure_rm_containerregistrywebhook
-./ansible-pr.sh azure_rm_containerregistrywebhook_facts
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "--- Updating Module: $line"
+    ./ansible-pr.sh $line
+done < ~/ansible-hatchery/current-prs.txt
