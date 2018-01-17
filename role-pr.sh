3169768e4090a88f9a04ba475b921eb196b2a0c4
zikalino <zikalino@microsoft.com>
2018-01-17 13:22:32 +0800

    fixed role-pr.sh

--- role-pr.sh
+++ role-pr.sh
@@ -10,7 +10,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "module: $line"
     cp -rfv ~/ansible-hatchery/all/modules/$line.py ~/azure_preview_modules/library
     cp -rfv ~/ansible-hatchery/all/tests/$line ~/azure_preview_modules/tests/integration/targets
-done < current-role.txt
+done < ~/ansible-hatchery/current-role.txt
 
 git add -A
 git commit -m "updated modules"
