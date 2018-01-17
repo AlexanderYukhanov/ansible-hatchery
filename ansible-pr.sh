70c76387fb49ca1ea5491fc2c56554ea8ca6a447
zikalino <zikalino@microsoft.com>
2018-01-17 13:30:49 +0800

    cleaned up scripts

--- ansible-pr.sh
+++ ansible-pr.sh
@@ -7,8 +7,8 @@ fi
 
 cd ~/$1
 git pull
-cp ~/ansible-hatchery/role/modules/$1.py ~/$1/lib/ansible/modules/cloud/azure/$1.py
-cp -v -r ~/ansible-hatchery/role/tests/$1 ~/$1/test/integration/targets
+cp ~/ansible-hatchery/all/modules/$1.py ~/$1/lib/ansible/modules/cloud/azure/$1.py
+cp -v -r ~/ansible-hatchery/all/tests/$1 ~/$1/test/integration/targets
 git add -A
 git commit -m "updates to $1"
 git push https://$GIT_USER:$GIT_PASSWORD@github.com/VSChina/ansible.git refs/heads/$1:refs/heads/$1
