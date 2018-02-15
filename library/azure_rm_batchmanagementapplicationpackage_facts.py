5e523d7a0a0393965d135172bfbf94a0b6ec5be7
zikalino <zikalino@microsoft.com>
2018-02-15 15:05:34 +0800

    batch management fixes

--- library/azure_rm_batchmanagementapplicationpackage_facts.py
+++ library/azure_rm_batchmanagementapplicationpackage_facts.py
@@ -91,7 +91,7 @@ from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 try:
     from msrestazure.azure_exceptions import CloudError
     from msrestazure.azure_operation import AzureOperationPoller
-    from azure.mgmt.batchmanagement import BatchManagementClient
+    from azure.mgmt.batch import BatchManagementClient
     from msrest.serialization import Model
 except ImportError:
     # This is handled in azure_rm_common
