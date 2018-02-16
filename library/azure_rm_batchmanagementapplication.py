7af0b313285774d839cad5e35dd383e195a88de3
zikalino <zikalino@microsoft.com>
2018-02-16 13:51:27 +0800

    fixing batch management application

--- library/azure_rm_batchmanagementapplication.py
+++ library/azure_rm_batchmanagementapplication.py
@@ -256,7 +256,7 @@ class AzureRMApplication(AzureRMModuleBase):
                                                         application_id=self.application_id)
             found = True
             self.log("Response : {0}".format(response))
-            self.log("Application instance : {0} found".format(response.application_id))
+            self.log("Application instance : {0} found".format(response.id))
         except CloudError as e:
             self.log('Did not find the Application instance.')
         if found is True:
